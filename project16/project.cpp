a66dddc0f1ab301364083e175e7788a5e242c947
Chapalaha <93518600+Chapalaha@users.noreply.github.com>
2021-11-25 19:13:19 +0300

    Update project.cpp

--- project16/project.cpp
+++ project16/project.cpp
@@ -1,172 +1,127 @@
-﻿
 #include <iostream>
-
 using namespace std;
+
+void clearCon() {
+	system("cls");
+	cout << "Консоль очищенна :)" << endl;
+	exit(0);	
+}
+
+void changeColorCon() {
+	system("color A0");
+}
+
+void changeColorText() {
+	system("color 0A");
+}
+
+void getSizeInt() {
+	cout << "Размер типа данных INT: " << sizeof(int) << " байта";
+}
+
+void getSizeChar() {
+	cout << "Размер типа данных CHAR: " << sizeof(char) << " байта";
+}
+
+void getSizeDouble() {
+	cout << "Размер типа данных DOUBLE: " << sizeof(double) << " байта";
+}
+
+void getSizeFloat() {
+	cout << "Размер типа данных FLOAT: " << sizeof(float) << " байта";
+}
+
+void getSizeBool() {
+	cout << "Размер типа данных BOOL: " << sizeof(bool) << " байта";
+}
+
+
+void showMessage() {
+	cout << "“Привет, я функция”";
+}
+
+void showSquare() {
+	short length_sq = 5;
+	short lenght_sq2 = length_sq;
+	cout << endl;
+	while (lenght_sq2 != 0) {
+		short lenght_sq3 = length_sq;
+		while (lenght_sq3 != 0) {
+			cout << "#" << " ";
+			lenght_sq3 -= 1;
+		}
+		cout << endl;
+		lenght_sq2 -= 1;
+	}
+}
+
 
 int main()
 {
-    setlocale(0, "");
-   
-    int a_1 = 111;
-    int a_2 = 2213;
-    int a_3 = 3124;
-    int a_4 = 21;
-    int a_5 = 143;
-    int a_6 = 345;
-    int a_7 = 837;
-    int a_8 = 736;
-    int a_9 = 221;
-    int a_10 = 81;
+	setlocale(0, "");
+	int number_Fun;
+	cout << "[ + ] Программа - “Функции”" << endl << endl;
+	cout << "[1] Очищение консоли" << endl;
+	cout << "[2] Изменение цвета фона консоли" << endl;
+	cout << "[3] Изменение цвета текста консоли" << endl;
+	cout << "[4] Вывод размера типа данных INT" << endl;
+	cout << "[5] Вывод размера типа данных CHAR" << endl;
+	cout << "[6] Вывод размера типа данных DOUBLE" << endl;
+	cout << "[7] Вывод размера типа данных FLOAT" << endl;
+	cout << "[8] Вывод размера типа данных BOOL" << endl;
+	cout << "[9] Вывод сообщения “Привет, я функция”" << endl;
+	cout << "[10] Вывод квадрата 5x5" << endl << endl;
+	cout << "Выберите функцию: ";
+	
+	cin >> number_Fun;
+	switch (number_Fun)
+	{
+	case 1: 
+		clearCon();	
+		  break;
 
-    cout << "a_1= " << a_1 << " , тип int, размер 4б" << endl;
-    cout << "a_2= " << a_2 << " , тип int, размер 4б" << endl;
-    cout << "a_3= " << a_3 << " , тип int, размер 4б" << endl;
-    cout << "a_4= " << a_4 << " , тип int, размер 4б" << endl;
-    cout << "a_5= " << a_5 << " , тип int, размер 4б" << endl;
-    cout << "a_6= " << a_6 << " , тип int, размер 4б" << endl;
-    cout << "a_7= " << a_7 << " , тип int, размер 4б" << endl;
-    cout << "a_8= " << a_8 << " , тип int, размер 4б" << endl;
-    cout << "a_9= " << a_9 << " , тип int, размер 4б" << endl;
-    cout << "a_10= " << a_10 << " , тип int, размер 4б" << endl;
-    cout << endl;
+	case 2:	
+		changeColorCon();
+	break;
 
-    char b_1 = 'a';
-    char b_2 = 'd';
-    char b_3 = 's';
-    char b_4 = 'e';
-    char b_5 = 'v';
-    char b_6 = 'm';
-    char b_7 = 'u';
-    char b_8 = 'n';
-    char b_9 = 'h';
-    char b_10 = 'l';
+	case 3:	
+		changeColorText();
+	break;
 
-    cout << "b_1= " << b_1 << " , тип char, размер 1б" << endl;
-    cout << "b_2= " << b_2 << " , тип char, размер 1б" << endl;
-    cout << "b_3= " << b_3 << " , тип char, размер 1б" << endl;
-    cout << "b_4= " << b_4 << " , тип char, размер 1б" << endl;
-    cout << "b_5= " << b_5 << " , тип char, размер 1б" << endl;
-    cout << "b_6= " << b_6 << " , тип char, размер 1б" << endl;
-    cout << "b_7= " << b_7 << " , тип char, размер 1б" << endl;
-    cout << "b_8= " << b_8 << " , тип char, размер 1б" << endl;
-    cout << "b_9= " << b_9 << " , тип char, размер 1б" << endl;
-    cout << "b_10= " << b_10 << " , тип char, размер 1б" << endl;
-    cout << endl;
+	case 4:	
+		getSizeInt();
+	break;
 
-    short c_1 = 1;
-    short c_2 = 2;
-    short c_3 = 3;
-    short c_4 = 4;
-    short c_5 = 5;
-    short c_6 = 6;
-    short c_7 = 7;
-    short c_8 = 8;
-    short c_9 = 9;
-    short c_10 = 10;
+	case 5:	
+		getSizeChar();
+	break;
 
-    cout << "c_1= " << c_1 << " , тип short, размер 2б" << endl;
-    cout << "c_2= " << c_2 << " , тип short, размер 2б" << endl;
-    cout << "c_3= " << c_3 << " , тип short, размер 2б" << endl;
-    cout << "c_4= " << c_4 << " , тип short, размер 2б" << endl;
-    cout << "c_5= " << c_5 << " , тип short, размер 2б" << endl;
-    cout << "c_6= " << c_6 << " , тип short, размер 2б" << endl;
-    cout << "c_7= " << c_7 << " , тип short, размер 2б" << endl;
-    cout << "c_8= " << c_8 << " , тип short, размер 2б" << endl;
-    cout << "c_9= " << c_9 << " , тип short, размер 2б" << endl;
-    cout << "c_10= " << c_10 << " , тип short, размер 2б" << endl;
-    cout << endl;
+	case 6:	
+		getSizeDouble();	
+	break;
 
-    long d_1 = 11211;
-    long d_2 = 2212313;
-    long d_3 = 3124;
-    long d_4 = 21231;
-    long d_5 = 143213;
-    long d_6 = 34135;
-    long d_7 = 83567;
-    long d_8 = 76736;
-    long d_9 = 22431;
-    long d_10 = 82341;
+	case 7:	
+		getSizeFloat();	
+	break;
 
-    cout << "d_1= " << d_1 << " , тип long, размер 4б" << endl;
-    cout << "d_2= " << d_2 << " , тип long, размер 4б" << endl;
-    cout << "d_3= " << d_3 << " , тип long, размер 4б" << endl;
-    cout << "d_4= " << d_4 << " , тип long, размер 4б" << endl;
-    cout << "d_5= " << d_5 << " , тип long, размер 4б" << endl;
-    cout << "d_6= " << d_6 << " , тип long, размер 4б" << endl;
-    cout << "d_7= " << d_7 << " , тип long, размер 4б" << endl;
-    cout << "d_8= " << d_8 << " , тип long, размер 4б" << endl;
-    cout << "d_9= " << d_9 << " , тип long, размер 4б" << endl;
-    cout << "d_10= " << d_10 << " , тип long, размер 4б" << endl;
-    cout << endl;
+	case 8:	
+		getSizeBool();	
+	break;
 
-    bool e_1 = true;
-    bool e_2 = false;
-    bool e_3 = true;
-    bool e_4 = true;
-    bool e_5 = false;
-    bool e_6 = true;
-    bool e_7 = false;
-    bool e_8 = true;
-    bool e_9 = true;
-    bool e_10 = false;
+	case 9:	
+		showMessage();
+	break;
 
-    cout << "e_1= " << e_1 << " , тип bool, размер 1б" << endl;
-    cout << "e_2= " << e_2 << " , тип bool, размер 1б" << endl;
-    cout << "e_3= " << e_3 << " , тип bool, размер 1б" << endl;
-    cout << "e_4= " << e_4 << " , тип bool, размер 1б" << endl;
-    cout << "e_5= " << e_5 << " , тип bool, размер 1б" << endl;
-    cout << "e_6= " << e_6 << " , тип bool, размер 1б" << endl;
-    cout << "e_7= " << e_7 << " , тип bool, размер 1б" << endl;
-    cout << "e_8= " << e_8 << " , тип bool, размер 1б" << endl;
-    cout << "e_9= " << e_9 << " , тип bool, размер 1б" << endl;
-    cout << "e_10= " << e_10 << " , тип bool, размер 1б" << endl;
-    cout << endl;
-
-    double f_1 = 1.83;
-    double f_2 = 4.34;
-    double f_3 = 6.32;
-    double f_4 = 23.54;
-    double f_5 = 65.21;
-    double f_6 = 6.98;
-    double f_7 = 12.53;
-    double f_8 = 43.98;
-    double f_9 = 9.38;
-    double f_10 = 2.53;
-
-    cout << "f_1= " << f_1 << " , тип double, размер 8б" << endl;
-    cout << "f_2= " << f_2 << " , тип double, размер 8б" << endl;
-    cout << "f_3= " << f_3 << " , тип double, размер 8б" << endl;
-    cout << "f_4= " << f_4 << " , тип double, размер 8б" << endl;
-    cout << "f_5= " << f_5 << " , тип double, размер 8б" << endl;
-    cout << "f_6= " << f_6 << " , тип double, размер 8б" << endl;
-    cout << "f_7= " << f_7 << " , тип double, размер 8б" << endl;
-    cout << "f_8= " << f_8 << " , тип double, размер 8б" << endl;
-    cout << "f_9= " << f_9 << " , тип double, размер 8б" << endl;
-    cout << "f_10= " << f_10 << " , тип double, размер 8б" << endl;
-    cout << endl;
-
-    float g_1 = 1.3;
-    float g_2 = 4.4;
-    float g_3 = 6.2;
-    float g_4 = 23.4;
-    float g_5 = 65.1;
-    float g_6 = 6.8;
-    float g_7 = 12.5;
-    float g_8 = 43.9;
-    float g_9 = 9.3;
-    float g_10 = 2.3;
-
-    cout << "g_1= " << g_1 << " , тип float, размер 4б" << endl;
-    cout << "g_2= " << g_2 << " , тип float, размер 4б" << endl;
-    cout << "g_3= " << g_3 << " , тип float, размер 4б" << endl;
-    cout << "g_4= " << g_4 << " , тип float, размер 4б" << endl;
-    cout << "g_5= " << g_5 << " , тип float, размер 4б" << endl;
-    cout << "g_6= " << g_6 << " , тип float, размер 4б" << endl;
-    cout << "g_7= " << g_7 << " , тип float, размер 4б" << endl;
-    cout << "g_8= " << g_8 << " , тип float, размер 4б" << endl;
-    cout << "g_9= " << g_9 << " , тип float, размер 4б" << endl;
-    cout << "g_10= " << g_10 << " , тип float, размер 4б" << endl;
-
-    return 0;
-    int _; cin >> _;
+	case 10:	
+		showSquare();	
+	break;
+	default:
+	{
+		system("cls");
+		cout << "Вы ввели некорректное значение!!!";
+	}
+	break;
+	}
+	int _; cin >> _;
+	return 0;
 }
